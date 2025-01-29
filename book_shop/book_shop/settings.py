787d33ac97912438fb85a18c0bda6ccadb60141e
Артём Тренин <gruzzli.hook@yandex.ru>
2025-01-29 13:16:38 +0300

    Add media

--- book_shop/book_shop/settings.py
+++ book_shop/book_shop/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
     BASE_DIR / STATIC_URL
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
