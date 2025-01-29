6514482abbab9e7fce3656f6a005d30634a1d6f3
Артём Тренин <gruzzli.hook@yandex.ru>
2025-01-29 12:02:45 +0300

    Write a category class and localize it into Russian

--- book_shop/catalog/models.py
+++ book_shop/catalog/models.py
@@ -3,7 +3,23 @@ from core.models.base import BaseModel, PublishedModel, StockModel
 
 
 class Category(BaseModel, PublishedModel):
-    pass
+    title = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Tag(BaseModel):
