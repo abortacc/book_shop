ac8dade3257298a3dd26cd57ec3f6500cd9a4702
Артём Тренин <gruzzli.hook@yandex.ru>
2025-01-29 12:26:29 +0300

    Localize abstract models

--- book_shop/core/models/base.py
+++ book_shop/core/models/base.py
@@ -2,22 +2,34 @@ from django.db import models
 
 
 class PublishedModel(models.Model):
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
 
     class Meta:
         abstract = True
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Создано в'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Изменено в'
+    )
 
     class Meta:
         abstract = True
 
 
 class StockModel(models.Model):
-    stock = models.PositiveIntegerField(default=0)
+    stock = models.PositiveIntegerField(
+        default=0,
+        verbose_name='В наличии'
+    )
 
     @property
     def is_avaible(self):
