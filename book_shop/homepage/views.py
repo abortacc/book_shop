7e3a3d279c50d7da43cbe436f12058399c4fa556
Артём Тренин <gruzzli.hook@yandex.ru>
2025-02-08 16:13:29 +0300

    Replace the view-function with a more compact CBV

--- book_shop/homepage/views.py
+++ book_shop/homepage/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import render
 from django.db.models import Q, Prefetch
+from django.views.generic import ListView
 from catalog.models import Book, Category
 
 
-def index(request):
-    template = 'homepage/index.html'
-    book_list_new = Book.objects.select_related(
-        'author', 'category'
-    ).prefetch_related(
-        'tags'
-    ).filter(
-        (Q(is_published=True)
-        & Q(category__is_published=True))
-    ).order_by(
-        '-created_at'
-    )
+class IndexListView(ListView):
+    template_name = 'homepage/index.html'
+    context_object_name = 'book_list_new'
 
-    categories = Category.objects.prefetch_related(
-        Prefetch('book_set', queryset=Book.objects.filter(
-            is_published=True).order_by('-created_at')
+    def get_queryset(self):
+        return Book.objects.select_related(
+            'author', 'category'
+        ).prefetch_related(
+            'tags'
+        ).filter(
+            Q(is_published=True) & Q(category__is_published=True)
+        ).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.prefetch_related(
+            Prefetch(
+                'book_set',
+                queryset=Book.objects.filter(
+                    is_published=True
+                ).order_by('-created_at')
+            )
+        ).filter(
+            is_published=True,
+            is_on_main=True
         )
-    ).filter(
-        is_published=True,
-        is_on_main=True
-    )
-    context = {
-        'book_list_new': book_list_new,
-        'categories': categories
-    }
-    return render(request, template, context)
+
+        return context
