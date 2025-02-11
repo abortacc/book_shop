5f031f16ee67e49241a9524b5e6edf378e78957b
Артём Тренин <gruzzli.hook@yandex.ru>
2025-02-11 15:02:47 +0300

    Create a login and registration pages

--- book_shop/book_shop/settings.py
+++ book_shop/book_shop/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'catalog.apps.CatalogConfig',
+    'django_bootstrap5',
     'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -133,6 +134,10 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'homepage:index'
+
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- book_shop/book_shop/urls.py
+++ book_shop/book_shop/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls', namespace='homepage')),
-    path('catalog/', include('catalog.urls', namespace='catalog'))
+    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('homepage:index')
+        ),
+        name='registration',
+    ),
 ]
 
 if settings.DEBUG:
