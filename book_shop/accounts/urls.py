2273d6c8dc733a8158d017822843bd5a76ea9276
Артём Тренин <gruzzli.hook@yandex.ru>
2025-02-14 12:42:05 +0300

    Protecting and viewing profiles

--- book_shop/accounts/urls.py
+++ book_shop/accounts/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.ProfileTemplateView.as_view(), name='profile'),
     path(
         'edit-profile/',
         views.EditProfileUpdateView.as_view(),
         name='edit_profile'
     ),
+    path('<slug:username>/', views.ProfileTemplateView.as_view(), name='profile'),
 ]
--- book_shop/accounts/views.py
+++ book_shop/accounts/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import UpdateView, TemplateView
+from django.views.generic import UpdateView, DetailView
 from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
 from .forms import ProfileEditForm
 
 
 User = get_user_model()
 
 
-class ProfileTemplateView(TemplateView):
+class ProfileTemplateView(LoginRequiredMixin, DetailView):
+    model = User
     template_name = 'accounts/profile.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_own_profile'] = self.object == self.request.user
+        return context
 
 
 class EditProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = ProfileEditForm
     template_name = 'accounts/edit_profile.html'
-    success_url = reverse_lazy('accounts:profile')
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_success_url(self):
+        return reverse(
+            'accounts:profile',
+            kwargs={'username': self.request.user.username}
+        )
