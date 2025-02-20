6359e48260434f4ce3c37d1d7da62b041df7c910
Артём Тренин <gruzzli.hook@yandex.ru>
2025-02-20 13:15:50 +0300

    Create CustomUser model, Comment, Award, Order, Library

--- book_shop/accounts/models.py
+++ book_shop/accounts/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from core.models.base import BaseModel, PublishedModel
+from catalog.models import Book
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class CustomUser(AbstractUser):
+    rating = models.FloatField(
+        default=0.0,
+        blank=True,
+        verbose_name='Рейтинг'
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        default='users/avatars/def_avatar.png',
+        blank=True,
+        null=True
+    )
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model, BaseModel, PublishedModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пользователь'
+    )
+    text = models.TextField(max_length=2056, verbose_name='Текст')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'Комментарий пользователя {self.user} №{self.id}'
+
+
+class Award(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='awards',
+        verbose_name='Пользователь'
+    )
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(
+        blank=True,
+        max_length=256,
+        verbose_name='Описание'
+    )
+    received_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Получил'
+    )
+
+
+class Order(models.Model, BaseModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Пользователь'
+    )
+    books = models.ManyToManyField(
+        Book,
+        verbose_name='Заказы',
+    )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'заказы'
+
+    def __str__(self):
+        return f'Заказ {self.user.username} №{self.id}'
+
+
+class Library(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='library',
+        verbose_name='Пользователь'
+    )
+    books = models.ManyToManyField(
+        Book,
+        related_name='purchased_by',
+        verbose_name='Книги'
+    )
