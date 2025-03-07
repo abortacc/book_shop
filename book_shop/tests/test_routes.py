4b296a03b080ae305a7c7642064bcec23a12afef
Артём Тренин <gruzzli.hook@yandex.ru>
2025-03-07 14:41:55 +0300

    Create comment access tests

--- book_shop/tests/test_routes.py
+++ book_shop/tests/test_routes.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 from catalog.models import Book, Author, Category
+from accounts.models import Comment
 
 
 User = get_user_model()
@@ -57,3 +58,43 @@ class TestProfile(TestCase):
         url = reverse('accounts:edit_profile')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class TestComments(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='testUser')
+        cls.second_user = User.objects.create(username='testUser2')
+        cls.category = Category.objects.create(title='Коты', slug='cats')
+        cls.author = Author.objects.create(name='Автор')
+        cls.book = Book.objects.create(
+            title='Книга',
+            author=cls.author,
+            price=499,
+            category=cls.category
+        )
+        cls.comment = Comment.objects.create(
+            user=cls.user,
+            book=cls.book,
+            text='Текст комментария'
+        )
+
+    def test_edit_delete_comment_access(self):
+        user_statuses = (
+            (self.user, HTTPStatus.OK),
+            (self.second_user, HTTPStatus.FOUND)
+        )
+        for user, status in user_statuses:
+            for operation in (
+                'catalog:edit_comment',
+                'catalog:delete_comment'
+            ):
+                with self.subTest(name=user):
+                    self.client.force_login(user)
+                    url = reverse(
+                        operation,
+                        args=(self.book.id, self.comment.id)
+                    )
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
