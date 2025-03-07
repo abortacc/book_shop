33785c9e1a82d066f0c8a4098324b593e71257ab
Артём Тренин <gruzzli.hook@yandex.ru>
2025-03-07 14:14:14 +0300

    Test  availability of profile routes

--- book_shop/tests/test_routes.py
+++ book_shop/tests/test_routes.py
@@ -1,9 +1,13 @@
 from http import HTTPStatus
 
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 from catalog.models import Book, Author, Category
+
+
+User = get_user_model()
 
 
 class TestBookDetail(TestCase):
@@ -30,5 +34,26 @@ class TestBookDetail(TestCase):
 
     def test_book_detail_url_access(self):
         url = reverse('catalog:details', args=(self.book.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class TestProfile(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='testUser')
+        cls.second_user = User.objects.create(username='testUser2')
+
+    def test_profile_url_access(self):
+        self.client.force_login(self.user)
+        for arg in ((self.user.username,), (self.second_user.username,)):
+            url = reverse('accounts:profile', args=arg)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_edit_profile_url_access(self):
+        self.client.force_login(self.user)
+        url = reverse('accounts:edit_profile')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
