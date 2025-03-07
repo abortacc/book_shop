d77bd7055f2c78adb6561a01e0885072be2997ad
Артём Тренин <gruzzli.hook@yandex.ru>
2025-03-07 11:58:22 +0300

    Moved the test code

---
+++ book_shop/tests/test_content.py
@@ -0,0 +1,34 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from catalog.models import Book, Author, Category
+
+
+class TestBookDetail(TestCase):
+    TITLE = 'Название'
+    DESCRIPTION = 'Описание'
+    PAGE_COUNT = 200
+    PRICE = 3499
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = Author.objects.create(name='Автор')
+        cls.category = Category.objects.create(
+            title='Категория',
+            slug='category',
+        )
+        cls.book = Book.objects.create(
+            title=cls.TITLE,
+            description=cls.DESCRIPTION,
+            author=cls.author,
+            page_count=cls.PRICE,
+            price=cls.PRICE,
+            book_format='PB',
+        )
+
+    def test_book_detail(self):
+        url = reverse('catalog:details', args=(self.book.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
