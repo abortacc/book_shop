64b387faef09636f02a71c629dfdfdee5c84a250
Артём Тренин <gruzzli.hook@yandex.ru>
2025-04-20 18:07:19 +0300

    Create order model

--- book_shop/cart/models.py
+++ book_shop/cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+from core.models.base import BaseModel
+
+
+class Order(BaseModel):
+    name = models.CharField(max_length=128, verbose_name='Имя клиента')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    items = models.JSONField(verbose_name='Товары заказа')
+    subtotal = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Сумма'
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'заказы'
+
+    def __str__(self):
+        return f'Заказ от {self.name}'
