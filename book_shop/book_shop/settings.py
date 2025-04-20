dee3a2744cb6872da71e3d720fe01ce6474faaba
Артём Тренин <gruzzli.hook@yandex.ru>
2025-04-20 18:03:24 +0300

    Create new app cart

--- book_shop/book_shop/settings.py
+++ book_shop/book_shop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cart.apps.CartConfig',
     'accounts.apps.AccountsConfig',
     'core.apps.CoreConfig',
     'catalog.apps.CatalogConfig',
